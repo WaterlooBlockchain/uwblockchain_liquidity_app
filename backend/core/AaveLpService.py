8583081098e105dcd5507c372de16ff45fbf086a
BraydenRoyston <brayden.royston10@gmail.com>
2023-02-05 15:31:01 -0800

    refactor

--- backend/core/AaveLpService.py
+++ backend/core/AaveLpService.py
@@ -12,31 +12,26 @@ class AaveLpService(object):
         # instantiate Contract Service
         load_dotenv("./.env")
         cls.contractAddress = "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
+        cls.contractImplementationAddress = "0xc6845a5c768bf8d7681249f8927877efda425baf"
         try:
             cls.contractService = ContractService(
                 apiKey=os.getenv("API_KEY"), 
                 nodeAddress=os.getenv("NODE_ADDRESS"),
-                contractAddress=cls.contractAddress
+                contractAddress=cls.contractAddress,
+                implementationAddress=cls.contractImplementationAddress
             )
         except:
             raise Exception("Failed to create ContractService for AaveProtocolDataProvider contract. Verify your .env file.")
 
         # connect to the Aave contract
         try:
-            cls.contract = cls.contractService.connect()
+            cls.contract = cls.contractService.connectImplementation()
         except:
             raise Exception("Failed to connect to AaveProtocolDataProvider Contract.")
-        
-        # connect using implementation address ABI
-        try:
-            cls.implementationAddress = "0xc6845a5c768bf8d7681249f8927877efda425baf"
-            cls.contract = cls.contractService.connectWithImplementationAddress(cls.implementationAddress)
-        except:
-            raise Exception("Failed to retrieve implementation contract address from Aave contract.")
-    
+            
     def listenToEvents(cls) -> None:
         print("----------------- LISTENING TO EVENTS... -----------------")
-        eventFilter = cls.contract.events.Withdraw.createFilter(fromBlock='latest')
+        eventFilter = cls.contract.events.Withdraw.createFilter(fromBlock="latest")
         
         while True:
             for event in eventFilter.get_new_entries():
